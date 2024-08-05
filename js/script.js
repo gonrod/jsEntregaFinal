400b4440579a43f01bc426011d7c867708d3345e
Gonzalo Rodriguez <gonza248@gmail.com>
2024-08-05 01:07:10 -0300

    attempting to avoid CORS issue once again

--- js/script.js
+++ js/script.js
@@ -482,10 +482,10 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function initializeApp() {
-    fetch('jsEntregaFinal/categories.json')  // Ensure the path to categories.json is correct.
+    fetch('/categories.json')  // Ensure the path to categories.json is correct.
         .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not good: ' + response.statusText);
+                throw new Error('Network response was not good again: ' + response.statusText);
             }
             return response.json();
         })
